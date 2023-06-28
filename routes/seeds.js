cf778437d6a6dfd5b0f32a57990984912597c1a4
p333r <perespenkm@gmail.com>
2023-06-28 09:23:20 +0200

    Added functionality to seeds route

--- routes/seeds.js
+++ routes/seeds.js
@@ -5,25 +5,31 @@ const passport = require("passport");
 const db = CyclicDB(process.env.CYCLIC_DB);
 const seeds = db.collection("seeds");
 const Seed = require("../models/seed");
+const { jwtAuth } = require("../services/helpers");
 
 router.get("/", async function (req, res, next) {
   const allSeeds = await seeds.list().then((data) => data.results);
-  res.json(allSeeds);
+  const seedsMap = allSeeds.map((seed) => seed.key);
+  console.log(seedsMap);
+  const seedArray = [];
+  await async function () { seedsMap.forEach(async (seed) => {
+    let item = new Seed(seed);
+    console.log(item);
+    await item.get();
+    seedArray.push(item);
+  });
+  };
+  console.log("11111111111111111", seedArray);
+  res.json(seedArray);
 });
 
 // Create new seed
-router.post(
-  "/",
-  passport.authenticate("jwt", { session: false, failureRedirect: "/login" }),
-  isAdmin,
-  async function (req, res, next) {
-    console.log(req.body);
-    const { name, gelatinous, gramsPerJar, growTime, soakTime } = req.body;
-    const seed = new Seed(name, gelatinous, gramsPerJar, growTime, soakTime);
-    await seed.save();
-    res.redirect("/admin");
-  }
-);
+router.post("/", jwtAuth, isAdmin, async function (req, res, next) {
+  const { name, gelatinous, gramsPerJar, growTime, soakTime } = req.body;
+  const seed = new Seed(name, gelatinous, gramsPerJar, growTime, soakTime);
+  await seed.save();
+  res.redirect("/admin");
+});
 
 // Delete seed
 router.delete(
